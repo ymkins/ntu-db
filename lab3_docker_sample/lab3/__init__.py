4626d5f9fc34291d4d7a97c61ff32bbfa90ebede
Anton Lysenko <lysenko@qarea.com>
2023-03-21 11:57:09 +0200

    lab3: what is a __init__.py file comment

---
+++ lab3_docker_sample/lab3/__init__.py
@@ -0,0 +1,14 @@
+# The file "init.py" is a special file in Python that is used to mark a directory as a Python package.
+# When the Python interpreter encounters a directory containing an init.py file, it recognizes the
+# directory as a package and allows you to import modules from the package.
+
+# The init.py file can be empty, or it can contain Python code that initializes the package.
+# This code might include importing modules, defining functions or classes, or setting up
+# package-level configuration.
+
+# In addition to marking a directory as a package, the init.py file also controls the behavior
+# of the package when it is imported. For example, you can use the all variable in the init.py
+# file to specify which modules should be imported when the package is imported using the * syntax.
+
+# Overall, the init.py file is an important part of creating a well-structured Python package
+# and provides a way to organize and control the behavior of the package.
